1566e51d69782c41a8a6dfc25387a9bb05cf32fa
alswooy <jjang990312@naver.com>
2024-07-26 18:41:33 +0900

    주석 추가

--- src/main/java/com/fastcampus/customer/board/BoardDao.java
+++ src/main/java/com/fastcampus/customer/board/BoardDao.java
@@ -4,15 +4,15 @@ import java.util.List;
 import java.util.Map;
 
 public interface BoardDao {
-    BoardDto select(Integer bno) throws Exception;
-    int delete(Integer bno, String writer) throws Exception;
-    int insert (BoardDto board) throws Exception;
-    int update (BoardDto board) throws Exception;
-    int increaseViewCnt(Integer bno) throws Exception;
-//
-    List<BoardDto> selectPage(Map map) throws Exception;
-    List<BoardDto> selectAll() throws Exception;
-    int deleteAll()throws Exception;
-    int count() throws Exception;
-    String getServerTime()throws Exception;
+    BoardDto select(Integer bno) throws Exception; //번호로 select
+    int delete(Integer bno, String writer) throws Exception;//번호와 작성자의 정보로 삭제
+    int insert (BoardDto board) throws Exception;//글 작성
+    int update (BoardDto board) throws Exception;//글 수정
+    int increaseViewCnt(Integer bno) throws Exception; //조회수 증가
+
+    List<BoardDto> selectPage(Map map) throws Exception; //page수 확인
+    List<BoardDto> selectAll() throws Exception;//모든페이지확인
+    int deleteAll()throws Exception;//모든글 삭제
+    int count() throws Exception;//모든글수확인
+    String getServerTime()throws Exception; //서버연결테스트
 }
--- src/main/java/com/fastcampus/customer/board/BoardService.java
+++ src/main/java/com/fastcampus/customer/board/BoardService.java
@@ -1,4 +1,8 @@
 package com.fastcampus.customer.board;
 
 public interface BoardService {
+    int getCount()throws Exception; //글수 확인
+    int remove(Integer bno, String writer) throws Exception;
+    int write(BoardDto boardDto) throws Exception;
+    
 }
